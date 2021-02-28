ee2e9789b870ad5bfe3f1e53b57592bad41db9bc
Rainy99 <y@devzhou.cn>
2021-02-28 19:39:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,3 @@
 
 #添加额外软件包
 git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
-git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
-
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
